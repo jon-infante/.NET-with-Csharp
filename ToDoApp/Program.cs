a3f311ad411327cbb0765ebc166251f86b32ae46
Jon Infante <jonjoninfante@gmail.com>
2021-12-10 11:24:59 -0500

    todos

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using ToDo;
+
+Console.WriteLine("Welcome to ToDo List");
+Console.WriteLine("What would you like to do today");
+Console.WriteLine("1. Create a new todo item");
+Console.WriteLine("2. View my list");
+Console.WriteLine("3. Complete an item");
+
+ToDoItem item = new ToDoItem();
+item.IsDone = false;
+item.Note = "Eat Lunch";
+
+Console.WriteLine(item.Print());
+
+item.CompleteItem();
+
+Console.WriteLine(item.Print());
+
+
+
+
+
---
+++ ToDoApp/ToDoItem.cs
@@ -0,0 +1,33 @@
+namespace ToDo;
+
+//This is a class, blueprints for object
+//It is a data type
+//It has members:
+//1. Constructor - this special method creates the object wtih the class' shape
+//2. Fields - They hold values that pertains to the class
+//3. Properties - They're wrapper around fields to make it more secure
+//4. Methods - They define behaviors of a class (Functions that belong to the class)
+//classes have access modifiers -> something that controls access to that particular class [public]
+//public = anyone can access
+public class ToDoItem {
+    //Empty Constructor
+    public ToDoItem() {}
+
+    //Automatic default property with getter and setter [Think of react native setState]
+    public bool IsDone { get; set;}
+    public string Note { get; set;}
+
+    //Method
+    //Does not take any arguments
+    public void CompleteItem() {
+        this.IsDone = true; 
+    }
+    //Method signature goes: Access Modifier, return type, method name, paramaters/arguments
+    public string Print(){
+        //String interpolation
+        return $"Done: {this.IsDone} Note: {this.Note}";
+    }
+
+}
+// //instantiating a new class
+// ToDoItem newItem = new ToDoItem();
